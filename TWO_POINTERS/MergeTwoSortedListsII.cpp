2beee3cdeaa1dc358a10b423322fc872f6605019
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-04-13 23:40:12 +0530

    Create MergeTwoSortedListsII.cpp

---
+++ TWO_POINTERS/MergeTwoSortedListsII.cpp
@@ -0,0 +1,17 @@
+void Solution::merge(vector<int> &A, vector<int> &B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+   /* int m=A.size();
+    int n=B.size();
+        for(int k=m;k<m+n;k++){
+            A[k]=B[k-m];
+        }   */
+        for(int i=0;i<B.size();i++){
+            A.push_back(B[i]);
+        }
+        sort(A.begin(),A.end());
+        
+   // return A;
+}
