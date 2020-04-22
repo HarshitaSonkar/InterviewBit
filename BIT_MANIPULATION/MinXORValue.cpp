203bd689404b862eb7c886df8157375cbeed14bb
Harshita Sonkar <harshitasonkar31@gmail.com>
2020-04-22 23:29:12 +0530

    Create MinXORValue.cpp

---
+++ BIT_MANIPULATION/MinXORValue.cpp
@@ -0,0 +1,13 @@
+int Solution::findMinXor(vector<int> &A) {
+    
+    sort(A.begin(),A.end());
+
+    int res=A[0]^A[1];
+    for(int i=0;i<A.size();i++){
+             res=min(res,A[i]^A[i+1]);
+    
+    }
+
+    return res;
+    
+}
